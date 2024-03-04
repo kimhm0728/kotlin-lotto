22283c0c618078d3e764f10ac54201af52032aac
kimhm0728 <kimhm0728@naver.com>
2024-03-04 19:58:05 +0900

    style: 출력 스타일 변경

--- src/main/kotlin/lotto/utils/Retry.kt
+++ src/main/kotlin/lotto/utils/Retry.kt
@@ -1,12 +1,10 @@
 package lotto.utils
 
-private const val ERROR_MESSAGE = "[ERROR]"
-
 fun <T> retryWhileNoException(action: () -> T): T {
     return try {
         action()
     } catch (e: IllegalArgumentException) {
-        println("$ERROR_MESSAGE ${e.localizedMessage}")
+        println(e.localizedMessage)
         retryWhileNoException(action)
     }
 }
