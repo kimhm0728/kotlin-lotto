7cf0da6abb77137047ab0ba6a61625da5796566f
kimhm0728 <kimhm0728@naver.com>
2024-03-04 21:16:38 +0900

    refactor: 입력값 예외 처리 방법 수정

--- src/main/kotlin/lotto/view/InputView.kt
+++ src/main/kotlin/lotto/view/InputView.kt
@@ -7,19 +7,25 @@ object InputView {
     private const val INPUT_WINNING_LOTTO_NUMBERS = "지난 주 당첨 번호를 입력해 주세요."
     private const val INPUT_BONUS_NUMBER = "보너스 볼을 입력해 주세요."
     private const val LOTTO_NUMBER_DELIMITER = ", "
-    private const val INVALID_DIGIT = -1
+    private const val INVALID_DIGIT = "숫자만 입력할 수 있습니다. "
 
     fun readPurchasePrice(): Int {
         println(INPUT_PURCHASE_PRICE)
         val inputPurchasePrice = readln()
-        return inputPurchasePrice.validateAndConvertDigit()
+        return inputPurchasePrice.toIntOrNull() ?: run {
+            print(INVALID_DIGIT)
+            readPurchasePrice()
+        }
     }
 
     fun readManualLottoSize(): Int {
         lineBreak()
         println(INPUT_MANUAL_LOTTO_SIZE)
         val inputManualLottoSize = readln()
-        return inputManualLottoSize.validateAndConvertDigit()
+        return inputManualLottoSize.toIntOrNull() ?: run {
+            print(INVALID_DIGIT)
+            readManualLottoSize()
+        }
     }
 
     fun readManualLottos(manualLottoSize: Int): List<List<Int>> {
@@ -35,21 +41,25 @@ object InputView {
 
     private fun readLottoNumbers(): List<Int> {
         val inputLottoNumbers = readln()
-        return inputLottoNumbers.validateAndConvertDigitList()
+        return inputLottoNumbers.validateAndConvertDigitList() ?: run {
+            println(INVALID_DIGIT)
+            readLottoNumbers()
+        }
     }
 
     fun readBonusNumber(): Int {
         println(INPUT_BONUS_NUMBER)
         val inputBonusNumber = readln()
-        return inputBonusNumber.validateAndConvertDigit()
+        return inputBonusNumber.toIntOrNull() ?: run {
+            print(INVALID_DIGIT)
+            readBonusNumber()
+        }
     }
 
-    private fun String.validateAndConvertDigitList(): List<Int> {
-        return split(LOTTO_NUMBER_DELIMITER).map { it.validateAndConvertDigit() }
-    }
-
-    private fun String.validateAndConvertDigit(): Int {
-        return toIntOrNull() ?: INVALID_DIGIT
+    private fun String.validateAndConvertDigitList(): List<Int>? {
+        return split(LOTTO_NUMBER_DELIMITER).map {
+            it.toIntOrNull() ?: return null
+        }
     }
 
     private fun lineBreak() = println()
