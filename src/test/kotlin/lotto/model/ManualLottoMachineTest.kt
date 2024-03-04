1e96024babbefd86bad69a7b9075f376a0b144ac
kimhm0728 <kimhm0728@naver.com>
2024-03-04 20:05:57 +0900

    test: 테스트 클래스를 인터페이스 구현체별로 분리

--- src/test/kotlin/lotto/model/ManualLottoMachineTest.kt
+++ src/test/kotlin/lotto/model/ManualLottoMachineTest.kt
@@ -3,22 +3,22 @@ package lotto.model
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
-class LottoMachineTest {
+class ManualLottoMachineTest {
     private val lottoNumbers = listOf(1, 2, 3, 4, 5, 6)
-    private val lottoSize = 5
+    private val lottoSize = 2
     private val lottos =
-        LottoMachine { lottoSize -> List(lottoSize) { Lotto.create(lottoNumbers) } }
+        ManualLottoMachine(listOf(lottoNumbers, lottoNumbers))
             .generate(lottoSize)
 
     @Test
-    fun `로또의 번호를 임의로 지정해주면 해당 번호로 로또가 발행된다`() {
+    fun `수동 로또의 번호를 임의로 지정해주면 해당 번호로 로또가 발행된다`() {
         assertThat(lottos).allMatch { lotto ->
             lottoNumbers.all { LottoNumber.from(it) in lotto }
         }
     }
 
     @Test
-    fun `지정해준 로또의 개수만큼 로또가 발행된다`() {
+    fun `지정해준 수동 로또의 개수만큼 로또가 발행된다`() {
         val actual = lottos.size
         assertThat(actual).isEqualTo(lottoSize)
     }
